5c73349eef866e02b6fcb57f90f80802060328af
Utopian <edwi.castruita117@gmail.com>
2026-05-13 11:40:30 -0600

    imp: add colors to map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   vite: {
